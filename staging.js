4f30a864627bd0434abf047b41e9b23a2d4c2455
BlueDev180 <tommyuk_2004@yahoo.co.uk>
2025-08-10 09:02:18 +0100

    Create staging.js

---
+++ staging.js
@@ -0,0 +1,44 @@
+// Board & staging arcs for hybrid layout
+export const BOARD = { W:14, H:6, NECRO_X:[0,1], ENEMY_X:[12,13] };
+
+export const STAGING = {
+  frontArc: [
+    {x:4,y:2},{x:4,y:3},{x:5,y:2},{x:5,y:3},
+    {x:4,y:1},{x:4,y:4},{x:5,y:1},{x:5,y:4},{x:4,y:0},{x:4,y:5}
+  ],
+  midArc: [
+    {x:3,y:2},{x:3,y:3},{x:3,y:1},{x:3,y:4},{x:2,y:2},{x:2,y:3}
+  ],
+  rearArc: [
+    {x:2,y:1},{x:2,y:4},{x:2,y:0},{x:2,y:5},{x:3,y:0},{x:3,y:5}
+  ],
+  flankArcLeft:  [{x:3,y:0},{x:4,y:0},{x:5,y:0},{x:6,y:0}],
+  flankArcRight: [{x:3,y:5},{x:4,y:5},{x:5,y:5},{x:6,y:5}],
+  eliteArc: [{x:3,y:2},{x:3,y:3},{x:2,y:2},{x:2,y:3}],
+  necroPos: {x:1, y:2}
+};
+
+export const ENEMY_STAGING = {
+  frontArc: [{x:9,y:2},{x:9,y:3},{x:10,y:2},{x:10,y:3},{x:9,y:1},{x:9,y:4},{x:10,y:1},{x:10,y:4}],
+  midArc:   [{x:11,y:2},{x:11,y:3},{x:11,y:1},{x:11,y:4}],
+  rearArc:  [{x:12,y:2},{x:12,y:3},{x:12,y:1},{x:12,y:4}],
+  flankArcLeft:  [{x:10,y:0},{x:9,y:0}],
+  flankArcRight: [{x:10,y:5},{x:9,y:5}],
+  bossArc: [{x:12,y:2},{x:12,y:3}]
+};
+
+export const ROLE_ARCS = {
+  Warrior: ["frontArc","midArc"],
+  Golem: ["frontArc","eliteArc"],
+  Archer: ["rearArc","midArc"],
+  Wraith: ["flankArcLeft","flankArcRight","midArc"],
+  EliteMelee: ["eliteArc","frontArc","midArc"],
+  EliteRanged:["eliteArc","rearArc","midArc"],
+  EnemyMelee:["frontArc","midArc"],
+  EnemyArcher:["rearArc","midArc"]
+};
+
+export const SQUADS = {
+  Warrior:{size:3}, Archer:{size:2}, Wraith:{size:2}, Golem:{size:1},
+  EnemyMelee:{size:3}, EnemyArcher:{size:2}
+};
